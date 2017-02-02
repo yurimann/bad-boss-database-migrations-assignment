9c9fe9122d8b75a5b71bf5ebd6a07d5a202510cc
Yuri <yuri.manahan@gmail.com>
2017-02-02 12:57:07 -0500

    Locations table created, migrated, rolled back, and migrated again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202173025) do
+ActiveRecord::Schema.define(version: 20170202175517) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string  "name"
+    t.string  "address"
+    t.integer "capacity"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
